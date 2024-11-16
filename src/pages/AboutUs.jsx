6f4907e1463eaea95bc0afdc309313d6e282e316
akashmahlax <akashdalla407@gmail.com>
2024-11-16 19:58:56 +0530

    updated

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -27,7 +27,7 @@ const AboutUs = () => {
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 1 }}
         >
-          About DAV School
+          About Mahla School
         </motion.h2>
         <motion.p
           className="mt-4 text-lg text-gray-300"
